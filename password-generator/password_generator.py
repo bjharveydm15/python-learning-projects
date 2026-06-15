afd1783b80f552150310d08f0f8df1a589fad3c8
bjharveydm15 <bjharveydm15@gmail.com>
2026-06-15 19:12:54 +0800

    feat: added file saving

--- password-generator/password_generator.py
+++ password-generator/password_generator.py
@@ -1,94 +1,176 @@
 import string
 import random
+import json
+from pathlib import Path
 
 class PasswordGenerator:
     def __init__(self):
+        self.criteria = {
+            'has_upper': {
+                'bool': False,
+                'prompt': "Include uppercase letters? (y/n): ",
+                'implementation': string.ascii_uppercase
+            },
+            'has_number': {
+                'bool': False,
+                'prompt': "Include numbers? (y/n): ",
+                'implementation': string.digits
+            },
+            'has_symbol': {
+                'bool': False,
+                'prompt': "Include special characters? (y/n): ",
+                'implementation': string.punctuation
+            }
+        }
+        self.pass_count = 1
         self.length = 1
-        self.has_upper = False
-        self.has_digit = False
-        self.has_special_char = False
-        self.password = ''
+        self.new_passwords = []
+        self.stored_passwords = []
 
-    def ask_length(self):
-            while True:
-                try:
-                    self.length =  int(input("Enter password length: ").strip())
+    def get_criteria(self, asked):
+        get_values = []
 
-                    if self.length < 4:
-                        print('Password length must be at least 4.')
-                        continue
+        for key in self.criteria.keys():
+            get_values.append(self.criteria[key][asked])
 
-                    return
-                except ValueError:
-                    print('Please enter a valid password length.')
+        return get_values
 
-    def ask_uppercase(self):
-            while True:
-                y_or_n = input("Include uppercase letters? (y/n): ").strip()
+    def ask_criteria(self):
+        self.ask_password_count('How many passwords would you like to generate? ')
 
-                if y_or_n.lower() == 'y':
-                    self.has_upper = True
-                    return
-                elif y_or_n.lower() == 'n':
-                    return
+        prompts = self.get_criteria('prompt')
 
-                print('Please enter y or n only.')
+        for prompt, key in zip(prompts, self.criteria.keys()):
+            self.ask_y_or_n(prompt, key)
 
-    def ask_numbers(self):
-            while True:
-                y_or_n = input("Include numbers? (y/n): ").strip()
+        self.ask_length("Enter password length: ")
 
-                if y_or_n.lower() == 'y':
-                    self.has_digit = True
-                    return
-                elif y_or_n.lower() == 'n':
-                    return
+    def ask_password_count(self, prompt):
+        while True:
+            try:
+                generation_count = int(input(prompt))
 
-                print('Please enter y or n only.')
+                if generation_count < 1:
+                    print('Password length must at least 1.')
+                    continue
 
-    def ask_special_char(self):
-            while True:
-                y_or_n = input("Include special characters? (y/n): ").strip()
+                self.pass_count = generation_count
+                return
 
-                if y_or_n.lower() == 'y':
-                    self.has_special_char = True
-                    return
-                elif y_or_n.lower() == 'n':
-                    return
+            except ValueError:
+                print('Please enter a valid number.')
 
-                print('Please enter y or n only.')
+    def ask_y_or_n(self, prompt, key):
+        while True:
+            y_or_n = input(prompt).strip()
 
-    #generate password
+            if y_or_n.lower() == 'y':
+                self.criteria[key]['bool'] = True
+                return
+            elif y_or_n.lower() == 'n':
+                return
+
+            print('Please enter y or n only.')
+
+    def ask_length(self, prompt):
+        required = 1 + sum(criterion['bool'] for criterion in self.criteria.values())
+
+        while True:
+            try:
+                self.length = int(input(prompt).strip())
+
+                if self.length < required:
+                    print(f'Password length must be at least {required} characters long.')
+                    continue
+
+                return
+            except ValueError:
+                print('Please enter a valid password length.')
+
     def generate_password(self):
-        char_list = string.ascii_lowercase
-        password = [random.choice(string.ascii_lowercase)]
+        while len(self.new_passwords) < self.pass_count:
+            char_pool = string.ascii_lowercase
+            password = [random.choice(string.ascii_lowercase)]
 
-        if self.has_upper:
-            char_list += string.ascii_uppercase
-            password.append(random.choice(string.ascii_uppercase))
-        if self.has_digit:
-            char_list += string.digits
-            password.append(random.choice(string.digits))
-        if self.has_special_char:
-            char_list += string.punctuation
-            password.append(random.choice(string.punctuation))
+            implementations = self.get_criteria('implementation')
+            bools = self.get_criteria('bool')
 
-        while len(password) < self.length:
-            password.append(random.choice(char_list))
-        random.shuffle(password)
+            for implementation, implemented in zip(implementations, bools):
+                if implemented:
+                    char_pool += implementation
+                    password.append(random.choice(implementation))
 
-        self.password = ''.join(password)
+            while len(password) < self.length:
+                password.append(random.choice(char_pool))
+            random.shuffle(password)
 
-        return
+            unique_pass = ''.join(password)
+
+            if unique_pass not in self.new_passwords:
+                self.new_passwords.append(unique_pass)
 
     def show_password(self):
-        print(f'Generated password: {self.password}')
+        print('\nGenerated passwords:')
+
+        for password in self.new_passwords:
+            print(f'\t{password}')
+        print('\n')
+
+    def save(self):
+        while True:
+            y_or_n = input('Do you want to save the passwords generated? (y/n): ').strip()
+
+            if y_or_n.lower() == 'y':
+                if not Path('saved_passwords.json').exists():
+                    with open('saved_passwords.json', 'w') as file:
+                        json.dump(self.new_passwords, file, indent=4)
+                        return
+                while True:
+                    y_or_n = input('Overwrite existing file? (y/n): ').strip()
+
+                    if y_or_n.lower() == 'y':
+                        with open('saved_passwords.json', 'w') as file:
+                            json.dump(self.new_passwords, file, indent=4)
+                            return
+                    if y_or_n.lower() == 'n':
+                        self.stored_passwords.extend(self.new_passwords)
+
+                        with open('saved_passwords.json', 'w') as file:
+                            json.dump(self.stored_passwords, file, indent=4)
+                            return
+                    else:
+                        print('Please enter y or n only.')
+            elif y_or_n.lower() == 'n':
+                return
+            else:
+                print('Please enter y or n only.')
+
+    def load(self):
+        try:
+            with open('saved_passwords.json', 'r') as file:
+                self.stored_passwords = json.load(file)
+        except FileNotFoundError:
+            return
+
+        while True:
+            y_or_n = input('Do you want to see previous saved passwords? (y/n): ').strip()
+
+            if y_or_n.lower() == 'y':
+                print('\nStored Passwords:')
+                for password in self.stored_passwords:
+                    print(f'\t{password}')
+                print('\n')
+                return
+            elif y_or_n.lower() == 'n':
+                return
+            else:
+                print('Please enter y or n only.')
+
 
 if __name__ == '__main__':
-    generate_password = PasswordGenerator()
-    generate_password.ask_length()
-    generate_password.ask_uppercase()
-    generate_password.ask_numbers()
-    generate_password.ask_special_char()
-    generate_password.generate_password()
-    generate_password.show_password()
+    generate_passwords = PasswordGenerator()
+    generate_passwords.load()
+    generate_passwords.ask_criteria()
+    generate_passwords.generate_password()
+    generate_passwords.show_password()
+    generate_passwords.save()
